b9cf92d601e94246595990170c4aac806878ce33
rfcreaciones <168677725+rfcreaciones@users.noreply.github.com>
2026-01-24 12:11:29 -0600

    Add files via upload

--- js/sweetalert2.js
+++ js/sweetalert2.js
@@ -5,7 +5,7 @@
 Swal.fire({
 	// title:'Reserva el Día',
 	//text: '',
-	html:'<p class="textos-alert-inicio">Ana Rivera Sandoval <br> y<br> Ivan Rosales Trejo</p><br><p class="textos-alert-inicio-T3">Tenemos el honor de invitar a usted y a su apreciable familia al XV aniversario de nuestra hija:</p><br><p class="textos-alert-inicio-T2">Ayelen Rosales Rivera</p></div>',
+	html:'<p class="textos-alert-inicio">Ana Rivera Sandoval <br> e <br> Ivan Rosales Trejo</p><br><p class="textos-alert-inicio-T3">Tenemos el honor de invitar a usted y a su apreciable familia al XV aniversario de nuestra hija:</p><br><p class="textos-alert-inicio-T2">Ayelen Rosales Rivera</p></div>',
 	//icon:'success'
 	confirmButtonText:'Abrir Invitación',
 	confirmButtonColor: "#d6b2e7",
@@ -39,7 +39,7 @@ $('#ceremonia').click(function () {
 Swal.fire({
 	title:'Ubicación de la Ceremonía ',
 	//text: '',
-	html:'<p>Parroquia de Santa Isabel Chalma<br>Cuándo: 30 de Junio de 2024<br>Dirección:Manzana 004, 56935 Santa Isabel Chalma, Méx.</p><div class="contact-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1884.443951957966!2d-98.76400439800341!3d19.156383477114716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce3cba0f600bfd%3A0xcfeb02ed0401c784!2sCapilla%20de%20Santa%20Isabel!5e0!3m2!1ses-419!2smx!4v1713588646744!5m2!1ses-419!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>',
+	html:'<p>Parroquia de Santa Isabel Chalma<br>Cuándo: Sábado 21 de Febrero<br>Dirección:Manzana 004, 56935 Santa Isabel Chalma, Méx.</p><div class="contact-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1884.443951957966!2d-98.76400439800341!3d19.156383477114716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce3cba0f600bfd%3A0xcfeb02ed0401c784!2sCapilla%20de%20Santa%20Isabel!5e0!3m2!1ses-419!2smx!4v1713588646744!5m2!1ses-419!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>',
 	//icon:'success'
 	confirmButtonText:'Cerrar',
 	//footer:'Parroquia de Dulce Nombre de Jesus ',
@@ -52,7 +52,7 @@ $('#recepcion').click(function () {
 		title:'Ubicación de la Recepción ',
 		//text: '',
 	
-		html:'<p>Calle Campo Florido No. 10 Colonia Santa Isabel Chalma Amecameca Edo.Mex.</p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d235.55362342303903!2d-98.7641372086852!3d19.157693202598626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2smx!4v1714763394986!5m2!1ses-419!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>',
+		html:'<p>Calle Campo Florido No. 10 Colonia Santa Isabel Chalma Amecameca Edo.Mex.</p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d313.14731617087676!2d-98.76241955056716!3d19.15982624822296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce3df5c7858679%3A0xfbe682b254b7c060!2sSal%C3%B3n%20el%20fandango!5e1!3m2!1ses-419!2smx!4v1769271678533!5m2!1ses-419!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>',
 		//icon:'success'
 		confirmButtonText:'Cerrar',
 		//footer:'Parroquia de Dulce Nombre de Jesus ',
